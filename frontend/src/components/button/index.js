6bdf4b101b0959e5c8ba3eff08a78347b3052e0d
orastvorov <orastvorov@waverleysoftware.com>
2022-09-21 12:29:46 +0300

    GH-16: update header ui

---
+++ frontend/src/components/button/index.js
@@ -0,0 +1,15 @@
+import { styled } from '@mui/material/styles'
+import { default as DefaultButton } from '@mui/material/Button'
+
+const Button= styled(DefaultButton)`
+  color: #fff;
+  padding: 0;
+  background-color: #333333;
+  font-size: 14px;
+  min-height: 40px;
+  text-transform: none;
+  border-radius: 6px;
+  box-shadow: none;
+`;
+
+export default Button
--- frontend/src/components/header/index.js
+++ frontend/src/components/header/index.js
@@ -4,8 +4,11 @@ import Tabs from '@mui/material/Tabs'
 import Tab from '@mui/material/Tab'
 import { styled } from '@mui/system'
 import { tabUnstyledClasses } from '@mui/base/TabUnstyled';
+import Grid from '@mui/material/Grid'
 import { useStore } from '../../store'
 import Logo from '../logo'
+import RoboDesignation from '../roboDesignation'
+import Button from '../button'
 import './style.css'
 
 const TabStyled = styled(Tab)`
@@ -16,6 +19,8 @@ const TabStyled = styled(Tab)`
   font-size: 12px;
   line-height: 14px;
   letter-spacing: 0.1em;
+  min-height: auto;
+  padding: 4px 0;
 
   &.${tabUnstyledClasses.selected} {
     color: #333333;
@@ -30,6 +35,10 @@ const Header = observer(() => {
     setActiveTab(newValue);
   };
 
+  const handleGuideClick = () => {
+    console.log('Guide button click')
+  }
+
   return (
     <header className='header'>
       <Logo />
@@ -39,10 +48,24 @@ const Header = observer(() => {
         TabIndicatorProps={{
           hidden: true
         }}
+        sx={{
+          marginLeft: '35px',
+          minHeight: 'auto'
+        }}
       >
         <TabStyled label='Dashboard' />
         <TabStyled label='Settings' />
       </Tabs>
+      <Grid sx={{marginLeft: 'auto', width: 'auto'}} container>
+        <Button
+          variant='contained'
+          sx={{ marginRight: '16px' }}
+          onClick={handleGuideClick}
+        >
+          Guide
+        </Button>
+        <RoboDesignation />
+      </Grid>
     </header>
   )
 })
