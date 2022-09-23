7d019b726f9cc0d1af4b54151fbb04b8092994db
orastvorov <orastvorov@waverleysoftware.com>
2022-09-23 13:03:01 +0300

    GH-21: update video controls ui

--- frontend/src/VideoControls/components/CameraControls/index.js
+++ frontend/src/VideoControls/components/CameraControls/index.js
@@ -1,7 +1,6 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { observer } from 'mobx-react'
 import { useStore } from '../../../store'
-import { lightBlue } from '@mui/material/colors'
 import { Radio, RadioGroup, FormControlLabel, FormControl } from '@mui/material'
 
 const CameraControls = observer(() => {
@@ -14,7 +13,7 @@ const CameraControls = observer(() => {
     }
   }
 
-  const cameraLabel = (name) => {
+  const cameraLabel = (name, marginLeft = 0) => {
     return (
       <FormControlLabel
         labelPlacement='start'
@@ -23,35 +22,35 @@ const CameraControls = observer(() => {
           <Radio
             size='small'
             sx={{
-              color: lightBlue[800],
+              color: '#979797',
               '&.Mui-checked': {
-                color: lightBlue[600]
+                color: '#ffffff',
               }
             }}
           />
         }
         label={name.toUpperCase()}
+        sx={{ marginLeft, '.MuiFormControlLabel-label': { fontSize: '12px' } }}
+        fontSize='12px'
       />
     )
   }
 
   return (
-    <Fragment>
-      <FormControl>
-        <RadioGroup
-          row
-          aria-labelledby='camera-radio-buttons-group-label'
-          defaultValue='rgb'
-          name='radio-buttons-group'
-          value={selectedMode}
-          onChange={handleCameraModeChange}
-        >
-          {cameraLabel('rgb')}
-          {cameraLabel('depth')}
-          {cameraLabel('sim')}
-        </RadioGroup>
-      </FormControl>
-    </Fragment>
+    <FormControl sx={{ marginLeft: '32px' }}>
+      <RadioGroup
+        row
+        aria-labelledby='camera-radio-buttons-group-label'
+        defaultValue='rgb'
+        name='radio-buttons-group'
+        value={selectedMode}
+        onChange={handleCameraModeChange}
+      >
+        {cameraLabel('rgb')}
+        {cameraLabel('depth', '22px')}
+        {cameraLabel('sim', '22px')}
+      </RadioGroup>
+    </FormControl>
   )
 })
 
