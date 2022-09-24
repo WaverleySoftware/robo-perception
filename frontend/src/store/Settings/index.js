d45c00566b7b9c5e54f5293f21e08c90f4b26191
Maxim Ovsiannikov <maxim@getreprise.com>
2022-09-24 13:27:50 +0300

    GH-17: adding new props/actions to the store; refactoring existing

--- frontend/src/store/Settings/index.js
+++ frontend/src/store/Settings/index.js
@@ -1,26 +1,52 @@
-import { observable, action, makeObservable } from 'mobx'
+import { observable, action, computed, makeObservable } from 'mobx'
+import { v4 as uuidv4 } from 'uuid'
 
 export default class Settings {
-  @observable robotTypes = [
-    'Legged',
-    'Wheeled',
-    'Manipulators',
-    'Wheeled/Legged+manipul',
-    'Flying'
+  @observable currentRobotTypeID = ''
+  @observable currentAppThemeID = ''
+
+  @observable widgets = [
+    { label: 'Screen', id: uuidv4(), selected: false },
+    { label: 'State', id: uuidv4(), selected: false },
+    { label: 'Additional actions', id: uuidv4(), selected: false },
+    { label: 'Speed', id: uuidv4(), selected: false }
   ]
-  @observable currentRobotType = ''
-  @observable currentAppTheme = ''
-  @observable appThemes = ['light', 'dark']
+  @observable robotTypes = [
+    { label: 'Legged', id: uuidv4() },
+    { label: 'Wheeled', id: uuidv4() },
+    { label: 'Manipulators', id: uuidv4() },
+    { label: 'Wheeled/Legged+manipul', id: uuidv4() },
+    { label: 'Flying', id: uuidv4() }
+  ]
+  @observable appThemes = [
+    { label: 'Light', id: uuidv4() },
+    { label: 'Dark', id: uuidv4() }
+  ]
+
+  @computed get currentRobotType() {
+    return this.robotTypes.find(type => type.id === this.currentRobotTypeID)
+  }
+
+  @computed get currentAppTheme() {
+    return this.appThemes.find(theme => theme.id === this.currentAppThemeID)
+  }
 
   constructor() {
     makeObservable(this)
   }
 
-  @action setRobotType = (type) => {
-    this.currentRobotType = type
+  @action setRobotType = (id) => {
+    this.currentRobotTypeID = id
   }
 
-  @action setAppTheme = (theme) => {
-    this.currentAppTheme = theme
+  @action setAppTheme = (id) => {
+    this.currentAppThemeID = id
+  }
+
+  @action toggleWidget = (id) => {
+    for (const widget of this.widgets) {
+      if (id !== widget.id) continue
+      widget.selected = !widget.selected
+    }
   }
 }
