6198e69719f93b0270ef73bfc5d325660ef31ef8
Maxim Ovsiannikov <maxim@getreprise.com>
2022-09-27 14:18:38 +0300

    GH-17: adding a bridge between "MobX" store and root "ThemeProvider" for themes switcher in the "Settings" page

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,27 +1,38 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { ThemeProvider, createTheme } from '@mui/material/styles'
 import red from '@mui/material/colors/red'
+import yellow from '@mui/material/colors/yellow'
 import './app.css'
-import { StoreProvider } from './store'
+import { StoreProvider, useStore } from './store'
 import Header from './components/header'
 import Dashboard from './components/dashboard'
 import Settings from './components/settings'
+import { observer } from 'mobx-react'
 
-const theme = createTheme({
-  palette: {
-    primary: red,
-  },
+const Root = observer(() => {
+  const {settingsStore: { currentAppTheme }} = useStore()
+  const theme = useMemo(() => createTheme({
+      palette: {
+        primary: currentAppTheme?.value === 'light' ? red : yellow,
+        mode: currentAppTheme?.value
+      },
+    }), [currentAppTheme]
+  )
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Header />
+      <Dashboard />
+      <Settings />
+    </ThemeProvider>
+  )
 })
 
 function App() {
   return (
     <div className='App'>
-      <StoreProvider>
-        <ThemeProvider theme={theme}>
-          <Header />
-          <Dashboard />
-          <Settings />
-        </ThemeProvider>
+      <StoreProvider colorTheme={'light'}>
+        <Root />
       </StoreProvider>
     </div>
   )
