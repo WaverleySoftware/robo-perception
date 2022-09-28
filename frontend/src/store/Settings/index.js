47c978b1d22fecf65719274a31a66071637d7d6a
Maxim Ovsiannikov <maxim@getreprise.com>
2022-09-28 18:40:24 +0300

    GH-17: using prettier with settings store file

--- frontend/src/store/Settings/index.js
+++ frontend/src/store/Settings/index.js
@@ -10,18 +10,18 @@ export default class Settings {
     { label: 'Screen', id: uuidv4(), selected: false },
     { label: 'State', id: uuidv4(), selected: false },
     { label: 'Additional actions', id: uuidv4(), selected: false },
-    { label: 'Speed', id: uuidv4(), selected: false }
+    { label: 'Speed', id: uuidv4(), selected: false },
   ]
   @observable robotTypes = [
     { label: 'Legged', id: uuidv4() },
     { label: 'Wheeled', id: uuidv4() },
     { label: 'Manipulators', id: uuidv4() },
     { label: 'Wheeled/Legged+manipul', id: uuidv4() },
-    { label: 'Flying', id: uuidv4() }
+    { label: 'Flying', id: uuidv4() },
   ]
   @observable appThemes = [
     { label: 'Light', id: uuidv4(), value: 'light' },
-    { label: 'Dark', id: uuidv4(), value: 'dark' }
+    { label: 'Dark', id: uuidv4(), value: 'dark' },
   ]
 
   constructor(rootStore, initialColorTheme) {
@@ -30,11 +30,11 @@ export default class Settings {
   }
 
   @computed get currentRobotType() {
-    return this.robotTypes.find(type => type.id === this.currentRobotTypeID)
+    return this.robotTypes.find((type) => type.id === this.currentRobotTypeID)
   }
 
   @computed get currentAppTheme() {
-    return this.appThemes.find(theme => theme.id === this.currentAppThemeID)
+    return this.appThemes.find((theme) => theme.id === this.currentAppThemeID)
   }
 
   @action setRobotName = (name) => {
@@ -50,7 +50,7 @@ export default class Settings {
   }
 
   @action setAppThemeByValue = (value) => {
-    this.currentAppThemeID = this.appThemes.find(theme => theme.value === value)?.id
+    this.currentAppThemeID = this.appThemes.find((theme) => theme.value === value)?.id
   }
 
   @action toggleWidget = (id) => {
