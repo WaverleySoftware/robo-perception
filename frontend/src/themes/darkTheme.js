77c0fcd2f7409c132029d607d4182b47af530d64
orastvorov <orastvorov@waverleysoftware.com>
2022-11-03 14:27:39 +0200

    GH-29: add default themes' configs

--- frontend/src/themes/darkTheme.js
+++ frontend/src/themes/darkTheme.js
@@ -2,10 +2,46 @@ import { createTheme } from '@mui/material/styles'
 
 const darkTheme = createTheme({
   palette: {
-    type: "dark",
+    type: 'dark',
     primary: {
-      main: "#000"
-    }
+      main: '#ffffff'
+    },
+    secondary: {
+      main: '#B7C0D9'
+    },
+    error: {
+      main: '#D9204C'
+    },
+    info: {
+      main: '#28BDEB'
+    },
+    success: {
+      main: '#6FCF41'
+    },
+    warning: {
+      main: '#FFAA04'
+    },
+    text: {
+      primary: '#ffffff',
+      secondary: '#B7C0D9'
+    },
+    button: {
+      disabled: '#565873'
+    },
+    background: {
+      main: '#17162D',
+      paper: 'rgba(51, 53, 83, 0.8)'
+    },
+  },
+  shape: {
+    cardBorderRadius: '16px',
+    formFieldBorderRadius: '6px',
+  },
+  typography: {
+    fontFamily: '"Poppins", "Arial", sans-serif',
+    fontWeightBold: 600,
+    fontWeightMedium: 500,
+    fontWeightRegular: 400,
   }
 });
 
--- frontend/src/themes/lightTheme.js
+++ frontend/src/themes/lightTheme.js
@@ -4,8 +4,44 @@ const lightTheme = createTheme({
   palette: {
     type: 'light',
     primary: {
-      main: '#4167B2'
-    }
+      main: '#262748'
+    },
+    secondary: {
+      main: '#B7C0D9'
+    },
+    error: {
+      main: '#D9204C'
+    },
+    info: {
+      main: '#28BDEB'
+    },
+    success: {
+      main: '#6FCF41'
+    },
+    warning: {
+      main: '#FFAA04'
+    },
+    text: {
+      primary: '#262748',
+      secondary: '#B7C0D9',
+    },
+    button: {
+      disabled: '#E1E6F4'
+    },
+    background: {
+      main: '#F2F4FC',
+      paper: 'rgba(253, 253, 255, 0.8)'
+    },
+  },
+  shape: {
+    cardBorderRadius: '16px',
+    formFieldBorderRadius: '6px',
+  },
+  typography: {
+    fontFamily: '"Poppins", "Arial", sans-serif',
+    fontWeightBold: 600,
+    fontWeightMedium: 500,
+    fontWeightRegular: 400,
   }
 });
 
