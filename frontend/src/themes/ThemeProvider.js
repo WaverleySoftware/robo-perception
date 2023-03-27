295340bea1079aac20d7ec6539b8dc2dc83ddc43
orastvorov <orastvorov@waverleysoftware.com>
2023-03-27 19:51:24 +0300

    GH-87: delete comments

--- frontend/src/themes/ThemeProvider.js
+++ frontend/src/themes/ThemeProvider.js
@@ -2,13 +2,6 @@ import React, { useMemo, useState } from 'react'
 import { createTheme, ThemeProvider as MuiThemeProvider } from '@mui/material/styles'
 import { getColorModeSettings, lightMode, darkMode } from './base'
 
-// TODO:
-// 1) setup theme settings by mode
-// 2) use variables for 'light' and 'dark'
-// 3) update components depends on mode
-// 4) update header design
-// 5) boxShadow to config ???
-
 export const ColorModeContext = React.createContext({})
 
 const ThemeProvider = (props) => {
